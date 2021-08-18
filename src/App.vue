350e596edc9ecbc4079c53db6bb2e3c96c6ca480
vike <Email>
2021-08-18 16:27:04 +0800

    新增分割线组件

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,5 @@
 <template>
-  <simpledivider />
-  <simpledivider dashed />
-  <div style="display:flex;justify-content: center">
-    <span>文字</span>
-    <simpledivider vertical />
-    <span>文字</span>
-    <simpledivider vertical />
-    <span>文字</span>
-  </div>
+
 </template>
  
 <script setup>
