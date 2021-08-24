8ecf34f42692fec4ed056b3ac067e24e0fdf6eb9
vike <Email>
2021-08-24 14:02:00 +0800

    新增input

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,19 @@
 <template>
-  
+  <simpleinput placeholder="请输入文本" v-model="values" />
+  <simpleinput placeholder="请输入文本" line v-model="values" />
+  <simpleinput placeholder="请输入文本" textarea v-model="values" />
+  <simpleinput placeholder="请输入文本" :maxlength="10" v-model="values" />
+  <simpleinput placeholder="请输入文本" disabled v-model="values" />
+  <simpleinput placeholder="请输入文本" readonly v-model="values" />
+
+  {{ values }}
 </template>
  
 <script setup>
-import simplebutton from "./components/Button.vue";
-
+import simpleinput from "./components/Input.vue";
+import { ref } from "vue";
+const values = ref("");
+// console.log(values.value)
 </script>
 <style>
 </style>
