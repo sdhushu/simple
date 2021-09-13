d28c6cc173b18cb6e7434d5ff9be14a42f6b2d4e
vike <Email>
2021-09-13 09:43:50 +0800

    新增选项卡组件

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,19 @@
 <template>
-
+ 
 </template>
  
 <script setup>
-import { ref } from "vue";
+import { reactive, ref } from "vue";
+import SimplesTabs from "./components/Tabs.vue";
+import SimplesTab from "./components/Tab.vue";
+import SimplesTabsItems from "./components/TabsItems.vue";
+import SimplesTabItem from "./components/TabItem.vue";
 
 </script>
 <style>
+* {
+  margin: 0;
+  padding: 0;
+}
+
 </style>
---
+++ src/components/TabItem.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="simples-tab-item" ref="ItemRef"><slot></slot></div>
+</template>
+ 
+<script>
+import { onMounted, ref } from "vue";
+export default {
+  name: "SimpleTabItem",
+  props: {
+      name:String
+  },
+  setup(props, context) {
+      let ItemRef = ref(null);
+      onMounted(()=>{
+          ItemRef.value.ariaLabel = props.name
+      })
+      return {ItemRef}
+  },
+};
+</script>
+<style>
+.simples-tab-item {
+    width: 100%;
+    height: 100%;
+    flex: none;
+    /* transform: translateX(-1280px); */
+}
+</style>
