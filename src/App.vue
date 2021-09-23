a5e3b0d5138e709b902b17bd6522d2bc1475b14e
vike <Email>
2021-09-23 16:44:12 +0800

    新增自定义指令样式 水波纹 点击特效

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,48 @@
 <template>
- 
+  <!-- 主题色按钮 -->
+  <simplesbutton v-water @click="handleClick">默认按钮</simplesbutton>
+  <simplesbutton type="primary" v-active>主要按钮</simplesbutton>
+  <simplesbutton type="info">信息按钮</simplesbutton>
+  <simplesbutton type="success">成功按钮</simplesbutton>
+  <simplesbutton type="warning">警告按钮</simplesbutton>
+  <simplesbutton type="danger">危险按钮</simplesbutton>
+  <!-- 文字按钮 -->
+  <simplesbutton text outline type="primary">外边框按钮</simplesbutton>
+  <simplesbutton text type="primary">纯文字按钮</simplesbutton>
+  <!-- 禁用状态 -->
+  <simplesbutton disabled>禁用按钮</simplesbutton>
+  <simplesbutton disabled text outline>禁用按钮</simplesbutton>
+  <simplesbutton disabled text>禁用按钮</simplesbutton>
+  <!-- 按钮尺寸 -->
+  <simplesbutton type="primary">常规按钮</simplesbutton>
+  <simplesbutton type="success" size="small">小型按钮</simplesbutton>
+  <simplesbutton type="warning" size="mini">迷你按钮</simplesbutton>
+  <simplesbutton type="danger" size="large">大型按钮</simplesbutton>
+  <!-- 块级按钮 -->
+  <simplesbutton block type="primary">块级按钮</simplesbutton>
+  <!-- 自定义颜色 -->
+  <simplesbutton color="#69dbaa" text-color="#eee">背景/文字</simplesbutton>
+  <simplesbutton
+    color="linear-gradient(to right, #69dbaa, #3a7afe)"
+    text-color="#fff"
+    >使用渐变</simplesbutton
+  >
+  <!-- 圆形按钮 -->
+  <simplesbutton type="primary" round></simplesbutton>
+  <!-- 加载按钮 -->
+  <simplesbutton type="primary" loading-type="circle">加载状态</simplesbutton>
+  <simplesbutton type="info" loading-type="rect">加载状态</simplesbutton>
+  <simplesbutton type="success" loading-type="disappear">加载状态</simplesbutton>
+  <simplesbutton type="warning" loading-type="cube">加载状态</simplesbutton>
+  <simplesbutton type="danger" loading-type="wave">加载状态</simplesbutton>
 </template>
- 
+
 <script setup>
-import { reactive, ref } from "vue";
-import SimplesTabs from "./components/Tabs.vue";
-import SimplesTab from "./components/Tab.vue";
-import SimplesTabsItems from "./components/TabsItems.vue";
-import SimplesTabItem from "./components/TabItem.vue";
-
-</script>
-<style>
-* {
-  margin: 0;
-  padding: 0;
+import simplesbutton from "./components/Button.vue";
+let handleClick = ()=>{
+  console.log('1111')
 }
+</script>
 
+<style>
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,19 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import './main.css'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.directive('active', {
+    mounted(el) {
+        el.classList.add('button')
+    }
+})
+
+app.directive('water', {
+    mounted(el) {
+        el.classList.add('water')
+    }
+})
+
+
+app.mount('#app')
