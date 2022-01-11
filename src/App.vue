9db7ba21e1c12f45a4ba429451c4990b680c79a3
vike <Email>
2022-01-11 14:38:58 +0800

    组件修改

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,19 @@
     <Option label="睡觉" :value="2"></Option>
   </Select>
 
+  <Select placeholder="请选择一个选项" v-model="value">
+    <Option label="吃饭" ></Option>
+    <Option label="睡觉" ></Option>
+  </Select>
+  <!-- <SliderVue v-model="value"/> -->
 </template>
 
 <script setup>
 import Select from './components/Select.vue';
 import Option from './components/Option.vue'
+import SliderVue from './components/Slider.vue';
 import { ref } from '@vue/reactivity';
-let value = ref('')
+let value = ref(3)
 </script>
 
 <style>
---
+++ src/components/Slider.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="slider">
+        <div class="slider-button"></div>
+    </div>
+</template>
+
+<script>
+export default {
+    props:{
+        modelValue:Number
+    },
+    setup(props) {
+        console.log(props.modelValue,'props')
+    },
+}
+</script>
+
+<style>
+.slider {
+    width: 100%;
+    height: 1px;
+    background-color: black;
+    position:absolute;
+}
+
+.slider-button {
+    width: 15px;
+    height: 15px;
+    border-radius: 50%;
+    background-color: skyblue;
+    position: relative;
+    transform: translateY(-50%);
+}
+</style>
